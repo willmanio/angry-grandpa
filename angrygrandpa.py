e7f3a9082f10ee4660e6bd516715d7909faae2f9
Christian E Willman <christianewillman@gmail.com>
2015-04-27 15:42:34 -0400

    Added random sleep to cloak network traffic

--- angrygrandpa.py
+++ angrygrandpa.py
@@ -1,41 +1,47 @@
-import getopt, sys, time
+import getopt, random, sys, time
 
 from soco import SoCo
 
 def main(argv):
   try:
-    opts, args = getopt.getopt(argv, "z:", [ "zone=" ])
+    opts, args = getopt.getopt(argv, "v:z:", [ "volume=", "zone=" ])
   except getopt.GetoptError:
     fail()
 
+  volume = None
   zone = None
 
   for o, a in opts:
-    if o in ("-z", "--zone"):
+    if o in ("-v", "--volume"):
+      volume = a
+    elif o in ("-z", "--zone"):
       zone = a
     else:
       fail()
 
-  if zone is None:
+  if volume is None or zone is None:
     fail()
   else:
-    getangry(SoCo(zone))
+    getangry(volume, SoCo(zone))
 
-def getangry(connection):
+def getangry(volume, zone):
   while True:
-    if connection.volume > 75:
-      print "Decreasing volume from %s" % connection.volume
-      connection.volume -= 5
+    if zone.volume > volume:
+      print "Decreasing volume from %s towards %s" % connection.volume, volume
+      zone.volume -= 5
+      time.sleep(30)
     else:
-      print "Everything's A-OK!"
-    time.sleep(10)
+      # Sleep a long time so we don't flood the network
+      sleep_minutes = random.randrange(3, 8)
+      print "Everything's A-OK, checking again in %s minutes" % sleep_minutes
+      time.sleep(sleep_minutes * 60)
   
 def fail():
   usage()
   sys.exit(2)
 
 def usage():
-  print 'Usage: ' + sys.argv[0] + ' -z <ipaddr>'
+  print 'Usage: ' + sys.argv[0] + ' -v <volume> -z <ipaddr>'
 
 if __name__ == "__main__":
   main(sys.argv[1:])
